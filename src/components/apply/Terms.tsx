2ac86365aac2e3e8b797ffe08e8eedbc132bafdb
김용민 <dydals3440@gmail.com>
2024-10-10 21:27:40 +0900

    feat: 약관동의 구현

--- src/components/apply/Terms.tsx
+++ src/components/apply/Terms.tsx
@@ -1,5 +1,80 @@
-function Terms() {
-  return <div>Terms</div>
+import { useCallback, useState, MouseEvent } from 'react'
+import Agreement from '@shared/Agreement'
+import FixedBottomButton from '@shared/FixedBottomButton'
+
+import { 약관목록 } from '@constants/apply'
+
+function Terms({ onNext }: { onNext: (terms: string[]) => void }) {
+  const [termsAgreements, setTermsAgreements] = useState(() => {
+    return 약관목록.reduce<Record<string, boolean>>(
+      (prev, term) => ({
+        ...prev,
+        [term.id]: false,
+      }),
+      {},
+    )
+  })
+
+  console.log(termsAgreements)
+
+  // ecery를 통해 모두 동의했는지 체크
+  const 모든약관이_동의되었는가 = Object.values(termsAgreements).every(
+    (동의여부) => 동의여부,
+  )
+
+  const handleAllAgreement = useCallback(
+    (_: MouseEvent<HTMLElement>, checked: boolean) => {
+      console.log('checked', checked)
+
+      setTermsAgreements((prevTerms) => {
+        // 01, 02
+        return Object.keys(prevTerms).reduce(
+          (prev, key) => ({
+            ...prev,
+            [key]: checked,
+          }),
+          {},
+        )
+      })
+    },
+    [],
+  )
+
+  return (
+    <div>
+      <Agreement>
+        <Agreement.Title
+          onChange={handleAllAgreement}
+          checked={모든약관이_동의되었는가}
+        >
+          약관에 모두 동의
+        </Agreement.Title>
+        {약관목록.map(({ id, title, link }) => (
+          <Agreement.Description
+            key={id}
+            link={link}
+            checked={termsAgreements[id]}
+            onChange={(_, checked) => {
+              setTermsAgreements((prevTerms) => ({
+                ...prevTerms,
+                [id]: checked,
+              }))
+            }}
+          >
+            {title}
+          </Agreement.Description>
+        ))}
+      </Agreement>
+      <FixedBottomButton
+        label="약관동의"
+        disabled={모든약관이_동의되었는가 === false}
+        onClick={() => {
+          // 내가 동의한 약관들의 key만 뺴옴.
+          onNext(Object.keys(termsAgreements))
+        }}
+      />
+    </div>
+  )
 }
 
 export default Terms
--- src/pages/Apply.tsx
+++ src/pages/Apply.tsx
@@ -7,9 +7,12 @@ import CardInfo from '@components/apply/CardInfo'
 function ApplyPage() {
   const [step, setStep] = useState(0)
 
+  const handleTermsChange = (values: string[]) => {
+    console.log('terms', values)
+  }
   return (
     <div>
-      {step === 0 ? <Terms /> : null}
+      {step === 0 ? <Terms onNext={handleTermsChange} /> : null}
       {step === 1 ? <BasicInfo /> : null}
       {step === 2 ? <CardInfo /> : null}
     </div>
