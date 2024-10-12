aa1d7aa52cce832dd2f46a270dd0f87976958ca0
김용민 <dydals3440@gmail.com>
2024-10-12 22:19:46 +0900

    feat: Polling 적용

--- src/components/apply/index.tsx
+++ src/components/apply/index.tsx
@@ -2,23 +2,69 @@ import Terms from '@components/apply/Terms'
 import BasicInfo from '@components/apply/BasicInfo'
 import CardInfo from '@components/apply/CardInfo'
 
-import { ApplyValues } from '@models/apply'
+import { APPLY_STATUS, ApplyValues } from '@models/apply'
+import { useState, useEffect } from 'react'
+import useUser from '@hooks/auth/useUser'
+import { useParams } from 'react-router-dom'
 
-function Apply({ step, onSubmit }: { step: number; onSubmit: () => void }) {
+function Apply({ onSubmit }: { onSubmit: (applyValues: ApplyValues) => void }) {
+  const user = useUser()
+  const { id } = useParams()
+  const [step, setStep] = useState(0)
+
+  // 데이터를 모음
+  // userId + cardId 랑 나머지 스프레드 연산자로 데이터를 받음.
+  const [applyValues, setApplyValues] = useState<Partial<ApplyValues>>({
+    userId: user?.uid,
+    cardId: id,
+  })
+
+  useEffect(() => {
+    // 값들이 다 채워졌음을 인지
+    // onSubmit시, 완성도니 데이터를 넘겨주어야함.
+    // Partial로 되어있기에 타입에러가 발생하는데 완성된 데이터라고 알려주어야함.
+    if (step === 3) {
+      onSubmit({
+        ...applyValues,
+        appliedAt: new Date(),
+        status: APPLY_STATUS.READY,
+      } as ApplyValues)
+    }
+  }, [applyValues, step, onSubmit])
+
   const handleTermsChange = (terms: ApplyValues['terms']) => {
-    console.log('terms', terms)
+    console.log(terms)
+    // terms {0: '01', 1:'02'}
+    // ...terms ['01', '02']
+    setApplyValues((prevValues) => ({
+      ...prevValues,
+      terms,
+    }))
+
+    setStep((prevStep) => prevStep + 1)
   }
 
   const handleBasicInfoChange = (
     infoValues: Pick<ApplyValues, 'salary' | 'payDate' | 'creditScore'>,
   ) => {
-    console.log('infoValues', infoValues)
+    setApplyValues((prevValues) => ({
+      ...prevValues,
+      ...infoValues,
+    }))
+
+    setStep((prevStep) => prevStep + 1)
   }
 
   const handleCardInfoChange = (
     cardInfoValues: Pick<ApplyValues, 'isHipass' | 'isMaster' | 'isRf'>,
   ) => {
-    console.log(cardInfoValues)
+    setApplyValues((prevValues) => ({
+      ...prevValues,
+      ...cardInfoValues,
+    }))
+
+    // 완료가 되었다는 것을 캐치해야하기에 useEffect를 사용.
+    setStep((prevStep) => prevStep + 1)
   }
 
   return (
--- src/pages/Apply.tsx
+++ src/pages/Apply.tsx
@@ -1,16 +1,51 @@
-import { useState } from 'react'
-
 import Apply from '@components/apply'
+import useApplyCardMutation from '@components/apply/hooks/useApplyCardMutation'
+import { useEffect, useState } from 'react'
+import usePollApplyStatus from '@components/apply/hooks/usePollApplyStatus'
+import { applyCard, updateApplyCard } from '@remote/apply'
+import useUser from '@hooks/auth/useUser'
+import { useParams } from 'react-router-dom'
+import { APPLY_STATUS } from '@models/apply'
 
 function ApplyPage() {
-  const [step, setStep] = useState(2)
+  // 폴링을 위한 플래그
+  const [readyToPoll, setReadyToPoll] = useState(false)
 
+  const { data } = usePollApplyStatus({
+    enabled: readyToPoll,
+  })
+
+  const user = useUser()
+  const { id } = useParams() as { id: string }
+
+  const { mutate } = useApplyCardMutation({
+    onSuccess: () => {
+      // 값이 추가되었을 떄 => 폴링 시작
+      setReadyToPoll(true)
+      console.log('카드 추가')
+    },
+    onError: () => {
+      // 실패했을 때 => 폴링 시작
+      window.history.back()
+    },
+  })
   // 데이터를 신청하는 쪽으로 관심사 분리
-  const handleSubmit = () => {
-    // TODO 카드 신청
-  }
+  // const handleSubmit = () => {
+  // }
+
+  useEffect(() => {
+    if (data) {
+      updateApplyCard({
+        cardId: id,
+        userId: user?.uid as string,
+        applyValues: {
+          status: APPLY_STATUS.REJECT,
+        },
+      })
+    }
+  }, [])
 
   // 데이터를 가져오는 쪽으로 관심사 분리
-  return <Apply step={step} onSubmit={handleSubmit} />
+  return <Apply onSubmit={mutate} />
 }
 export default ApplyPage
